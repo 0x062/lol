ea6b13c41e202962b38af6cb1d029a697c373926
＾Izumi Wakanabe＾ <38299646+0x062@users.noreply.github.com>
2025-05-27 04:03:39 +0700

    Update lol.js

--- lol.js
+++ lol.js
@@ -7,7 +7,7 @@ const { toBech32 } = require('@cosmjs/encoding');
 const { DirectSecp256k1HdWallet } = require('@cosmjs/proto-signing');
 const { SigningStargateClient } = require('@cosmjs/stargate');
 
-const RPC_ENDPOINT = "https://xion-rpc.f5nodes.com";
+const RPC_ENDPOINT = "https://xion-testnet-rpc.polkachu.com";
 const PREFIX = "xion";
 const AMOUNT_TO_SEND = "1000"; // 0.001 XION = 1000 uxion
 
